5641241ad20cbb89b62fa3217164c1ea0b9539b1
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-17 06:02:19 -0500

    Edit the Postform

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react'
 
 class Postform extends Component {
     state = {
-        
+
     }
   render() {
     return (
       <div>
         <h1>Add Post</h1>
+        <form>
+            <div>
+                <label>Title: </label><br/>
+                <input type="text" name="title"/>
+            </div>
+            <br/>
+            <div>
+                <label>Body: </label><br/>
+                <textarea name="body"/>
+            </div>
+            <br/>
+            <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
