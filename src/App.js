f7bfe1bc349b870d1bafe0c3e1b244b2878a00d2
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-17 05:55:17 -0500

    Create Postform component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Postform/>
           <Posts/>
           <a
             className="App-link"
---
+++ src/components/Postform.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+    state = {
+        
+    }
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+      </div>
+    )
+  }
+}
+
+
+export default Postform;
